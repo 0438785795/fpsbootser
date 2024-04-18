ea45c22dde06b63757af078c8aa3f469e7619d38
0438785795 <134633374+0438785795@users.noreply.github.com>
2024-04-18 20:51:08 +0100

    nocrash.lua

--- nocrash.lua
+++ nocrash.lua
@@ -57,7 +57,7 @@ if not _G.Settings then
             Destroy = true
         },
         Other = {
-            ["FPS Cap"] = 30, -- Set this true to uncap FPS
+            ["FPS Cap"] = 20, -- Set this true to uncap FPS
             ["No Camera Effects"] = true,
             ["No Clothes"] = true,
             ["Low Water Graphics"] = true,
